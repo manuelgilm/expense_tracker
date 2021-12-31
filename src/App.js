f775466ef73b646c78a64e8a6950cfc7fd45d942
Manuel Gil <manuelgilsitio@gmail.com>
2021-12-31 11:27:25 -0500

    log page has been added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css"
-import "./index.css"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
 import SignIn from "./components/SignIn"
-import SignUp from "./components/SignUp";
+import SignUp from "./components/SignUp"
 
 function App() {
   return (
+    <React.StrictMode>
+
     <Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>Financial Tracker  </Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-            </ul>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}>Financial Tracker</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route path="/" exact element={<SignIn/>}/>
+              <Route path="/sign-in" element={<SignIn/>}/>
+              <Route path="/sign-up" element={<SignUp/>}/>
+            </Routes>
+
           </div>
         </div>
-      </nav>
-
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-        <Routes>
-          <Route exact path='/' component={SignIn} />
-          <Route path="/sign-in" component={SignIn} />
-          <Route path="/sign-up" component={SignUp} />
-        </Routes>
-        </div>
       </div>
-    </div>
     </Router>
+    </React.StrictMode>
   );
 }
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../index.css"
 
 function SignUp(){
     return (
