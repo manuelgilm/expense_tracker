f0f07ca1d30a71ad3efc0eed160f721d54d355ea
Manuel Gil <manuelgilsitio@gmail.com>
2022-01-01 16:27:12 -0500

    Changes in signUp form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,28 +1,51 @@
 import React,{useState} from "react"
 
 function SignUp(){
+    const [newUser, setNewUser] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:""
+    })
+
+    function handleChange(event){
+        const {name, value} = event.target
+
+        setNewUser(preValue=>{
+            return {
+                ...preValue,
+                [name]:value
+            }
+        })
+    }
+    
+    function onSubmit(event){
+        event.preventDefault()
+        console.log(newUser)
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <h3>Sign Up</h3>
 
             <div className="form-group">
                 <label>First name</label>
-                <input type="text" className="form-control" placeholder="First name" />
+                <input type="text" className="form-control" placeholder="First name" value={newUser.firstName} name="firstName" onChange={handleChange} />
             </div>
 
             <div className="form-group">
                 <label>Last name</label>
-                <input type="text" className="form-control" placeholder="Last name" />
+                <input type="text" className="form-control" placeholder="Last name" value={newUser.lastName} name="lastName" onChange={handleChange}/>
             </div>
 
             <div className="form-group">
                 <label>Email address</label>
-                <input type="email" className="form-control" placeholder="Enter email" />
+                <input type="email" className="form-control" placeholder="Enter email" value={newUser.email} name="email" onChange={handleChange}/>
             </div>
 
             <div className="form-group">
                 <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" />
+                <input type="password" className="form-control" placeholder="Enter password" value={newUser.password} name="password" onChange={handleChange}/>
             </div>
 
             <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
